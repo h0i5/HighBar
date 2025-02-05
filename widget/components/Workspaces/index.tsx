7500f1d22f242b6b78f3d93f4a70fcd9910c38e2
HarshIyer <git@hi05.xyz>
2025-02-06 00:38:11 +0530

    fix return js for workspaces

--- widget/components/Workspaces/index.tsx
+++ widget/components/Workspaces/index.tsx
@@ -1,6 +1,5 @@
 import Hyprland from "gi://AstalHyprland";
 import { bind } from "astal";
-import { Variable } from "astal";
 
 function InactiveWorkspace() {
   return (
@@ -29,36 +28,42 @@ for (const client of hyprland.get_clients()) {
 }
 
 export default function Workspaces() {
-  return bind(hyprland, "workspaces").as((workspaces) => {
-    //sort workspaces
-    workspaces.sort((a, b) => a.get_id() - b.get_id());
-    return (
-      <box>
-        {workspaces.map((workspaces) => {
-          return bind(hyprland, "focused_workspace").as((focused_workspace) => {
-            if (focused_workspace.get_id() === workspaces.get_id()) {
-              return (
-                <button
-                  onClick={() => {
-                    focused_workspace.focus();
-                  }}
-                >
-                  <ActiveWorkspace />
-                </button>
+  return (
+    <box>
+      {bind(hyprland, "workspaces").as((workspaces) => {
+        //sort workspaces
+        workspaces.sort((a, b) => a.get_id() - b.get_id());
+        return (
+          <box>
+            {workspaces.map((workspaces) => {
+              return bind(hyprland, "focused_workspace").as(
+                (focused_workspace) => {
+                  if (focused_workspace.get_id() === workspaces.get_id()) {
+                    return (
+                      <button
+                        onClick={() => {
+                          focused_workspace.focus();
+                        }}
+                      >
+                        <ActiveWorkspace />
+                      </button>
+                    );
+                  }
+                  return (
+                    <button
+                      onClick={() => {
+                        workspaces.focus();
+                      }}
+                    >
+                      <InactiveWorkspace />
+                    </button>
+                  );
+                },
               );
-            }
-            return (
-              <button
-                onClick={() => {
-                  workspaces.focus();
-                }}
-              >
-                <InactiveWorkspace />
-              </button>
-            );
-          });
-        })}
-      </box>
-    );
-  });
+            })}
+          </box>
+        );
+      })}
+    </box>
+  );
 }
